a1031409830e2102386c606c0c61f28fdee9f7d6
NdigueSENE <senendigue@gmail.com>
2023-12-17 17:09:12 +0000

    refactoring

--- populate_base/mysql/app.py
+++ populate_base/mysql/app.py
@@ -1,14 +1,19 @@
 # installation du package python-binance
 # pip3 install python-binance
+import datetime
+import os
 
 import mysql.connector
-import datetime
 from binance.client import Client
-from mysql.connector import Error
 
-from constant import api_key, api_secret, BDNAME_MYSQL, TABLENAME_MYSQL, USER_MYSQL, PASSWORD_MYSQL, \
-    HOST_MYSQL, \
-    PORT_MYSQL
+api_key = os.getenv("api_key")
+api_secret = os.getenv("api_secret")
+BDNAME_MYSQL = os.getenv("MYSQL_DATABASE")
+TABLENAME_MYSQL = os.getenv("MYSQL_TABLENAME")
+USER_MYSQL = os.getenv("MYSQL_USER")
+PASSWORD_MYSQL = os.getenv("MYSQL_PASSWORD")
+HOST_MYSQL = os.getenv("MYSQL_HOST")
+PORT_MYSQL = os.getenv("MYSQL_PORT")
 
 client = Client(api_key=api_key, api_secret=api_secret, testnet=True)
 
@@ -31,15 +36,47 @@ def get_all_symbols(client):
     return data
 
 
+def check_mysql_connection_and_get_current_connexion():
+    max_attempts = 30
+    attempts = 0
+    connected = False
+    # Cette partie permet d'etre sur que le mysql est ready, parce que
+    # Docker ne garantit pas nécessairement l'ordre de démarrage des services, ce qui peut entraîner le démarrage de votre service Python (app) avant que le service de la base de données MySQL (db)
+    # ne soit prêt
+    import time
+    connection = None
+
+    while not connected and attempts < max_attempts:
+        try:
+            connection = mysql.connector.connect(host=HOST_MYSQL,
+                                                 port=PORT_MYSQL,
+                                                 database=BDNAME_MYSQL,
+                                                 user=USER_MYSQL,
+                                                 password=PASSWORD_MYSQL)
+            connected = True
+            connection.close()
+            print("MySQL is ready!")
+        except mysql.connector.Error as err:
+            print(f"Attempt {attempts + 1}: MySQL is not ready yet - Error: {err}")
+            attempts += 1
+            time.sleep(10)
+
+    if not connected:
+        print("Failed to connect to MySQL.")
+
+    return connection
+
+
 end_date = datetime.datetime.now()
-# On prends une historique de données de 90 jours glissants
-start_date = end_date - datetime.timedelta(days=90)  # 90 days ago
+# On prends une historique de données de 60 jours glissants
+start_date = end_date - datetime.timedelta(days=60)  # 60 days ago
 
 # Convert dates to milliseconds (required by Binance API)
 start_timestamp = int(start_date.timestamp() * 1000)
 end_timestamp = int(end_date.timestamp() * 1000)
 
 data = []
+print("LOG TO GET ALL MARCHES")
 # allSymbols = get_all_symbols(client)
 allSymbols = ["ETHBTC"]
 print("nombre de marches " + str(len(allSymbols)))
@@ -58,46 +95,34 @@ for symbol in allSymbols:
         data.append(price_data)
 
 print("sorti de boucle")
-# print(f"nombre de colonne = {len(data)}")
-try:
-    connection = mysql.connector.connect(host=HOST_MYSQL,
-                                         port=PORT_MYSQL,
-                                         database=BDNAME_MYSQL,
-                                         user=USER_MYSQL,
-                                         password=PASSWORD_MYSQL)
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(BDNAME_MYSQL))
-        cursor.execute("DROP TABLE IF EXISTS {}.{}".format(BDNAME_MYSQL, TABLENAME_MYSQL))
-        cursor.execute(
-            "CREATE TABLE " + str(BDNAME_MYSQL) + "." + str(
-                TABLENAME_MYSQL) + " (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-                                   "open_price FLOAT, "
-                                   "high_price FLOAT, "
-                                   "low_price FLOAT, "
-                                   "close_price FLOAT, "
-                                   "volume FLOAT, "
-                                   "quote_asset_volume FLOAT, "
-                                   "number_of_trades INT, "
-                                   "kline_open_time_parsed DATETIME,"
-                                   "kline_close_time_parsed DATETIME,"
-                                   "symbol VARCHAR(20))")
-        print("ok")
+connection = check_mysql_connection_and_get_current_connexion()
 
-        sql = """INSERT INTO {}.{}(open_price, high_price, low_price,close_price,""" \
-              """volume, quote_asset_volume, number_of_trades, """ \
-              """kline_open_time_parsed, kline_close_time_parsed, symbol) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""".format(
-            BDNAME_MYSQL, TABLENAME_MYSQL)
+if connection.is_connected():
+    db_Info = connection.get_server_info()
+    print("Connected to MySQL Server version ", db_Info)
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(BDNAME_MYSQL))
+    cursor.execute("DROP TABLE IF EXISTS {}.{}".format(BDNAME_MYSQL, TABLENAME_MYSQL))
+    cursor.execute(
+        "CREATE TABLE " + str(BDNAME_MYSQL) + "." + str(
+            TABLENAME_MYSQL) + " (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+                               "open_price FLOAT, "
+                               "high_price FLOAT, "
+                               "low_price FLOAT, "
+                               "close_price FLOAT, "
+                               "volume FLOAT, "
+                               "quote_asset_volume FLOAT, "
+                               "number_of_trades INT, "
+                               "kline_open_time_parsed DATETIME,"
+                               "kline_close_time_parsed DATETIME,"
+                               "symbol VARCHAR(20))")
+    print("ok")
 
-        cursor.executemany(sql, data)
-        connection.commit()
-    print("You're connected to database: ")
-except Error as e:
-    print("Error while connecting to MySQL", e)
-    connection.rollback()
-finally:
-    if connection.is_connected():
-        connection.close()
-        print("MySQL connection is closed")
+    sql = """INSERT INTO {}.{}(open_price, high_price, low_price,close_price,""" \
+          """volume, quote_asset_volume, number_of_trades, """ \
+          """kline_open_time_parsed, kline_close_time_parsed, symbol) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""".format(
+        BDNAME_MYSQL, TABLENAME_MYSQL)
+
+    cursor.executemany(sql, data)
+    connection.commit()
+    print("MySQL connection is closed")
