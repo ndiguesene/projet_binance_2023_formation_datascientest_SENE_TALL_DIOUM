e6b3e2fdf8d01947c85ad672aab1b5579ac9ff98
NdigueSENE <senendigue@gmail.com>
2023-12-28 09:54:20 +0000

    changement de OS docker to ubuntu:20.04

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 cd ./populate_base/mysql
-docker image build . -t projet_final/populate_into_mysql:0.0.1
+docker image build --no-cache . -t projet_final/populate_into_mysql:0.0.1
 
 cd ./../elasticsearch
-docker image build . -t projet_final/populate_into_elasticsearch:0.0.1
+docker image build --no-cache . -t projet_final/populate_into_elasticsearch:0.0.1
 
 cd ./../../api/
-docker image build . -t projet_final/api:0.0.1
+docker image build --no-cache . -t projet_final/api:0.0.1
 
 cd ./../models/
-docker image build . -t projet_final/model:0.0.1
+docker image build --no-cache . -t projet_final/model:0.0.1
 
 cd ../up
 
